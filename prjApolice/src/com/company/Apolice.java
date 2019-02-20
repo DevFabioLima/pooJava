bd127a79d3a3a9950597ed2a2c19a4e551ed79ae
LucasMartins99 <fabio.lucaslima@outlook.com>
2019-02-20 20:48:55 -0300

    exercicios aula 2

---
+++ prjApolice/src/com/company/Apolice.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Apolice {
+    String nomeSegurado;
+    int idade;
+    float valorPremio;
+
+    void Imprimir(){
+        System.out.println("o nome do segurado eh " + nomeSegurado);
+        System.out.println("a idade eh " + idade);
+        System.out.println("o valor do premio eh " + valorPremio);
+
+    }
+}
---
+++ prjApolice/src/com/company/ApoliceTeste.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class ApoliceTeste {
+
+	public static void main(String[] args) {
+		Apolice apolice = new Apolice();
+		apolice.valorPremio = 122;
+		apolice.idade = 25;
+		apolice.nomeSegurado = "Fabio Lucas";
+		apolice.Imprimir();
+	}
+}
+
